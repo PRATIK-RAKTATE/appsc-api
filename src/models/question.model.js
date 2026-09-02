1c86d8e03f9d09109bd1e0ba5c44cf6e485d5077
prashant girge <vikasgirge5656@gmail.com>
2026-09-02 18:56:44 +0530

    fix: bilingual fields and  question schemas

--- src/models/question.model.js
+++ src/models/question.model.js
@@ -1,29 +1,80 @@
 import mongoose, { Schema } from "mongoose";
 
-const questionSchema = new Schema(
+
+const bilingualSchema = new schema(
     {
-        subjectID: { 
-            type: Schema.Types.ObjectId,
-            ref: "Subject"
-        },
-        topicID: {
-            type: Schema.Types.ObjectId,
-            ref: "Topic"
-        },
-        subTopic: {
-            type: Schema.Types.ObjectId,
-            ref: "SubTopic"
-        },
-        question: {
-
-        },
-        option: [
-
-        ],
-        explaination: 
-
+       en: {
+        type: String,
+        required: true,
+        trim: true
+       },
+       te: {
+        type: String,
+        required: true,
+        trim: true
+       } 
     },
-    { timestamps: true }
+    { _id: false }
+);
+
+
+const optionSchema = new schema(
+    {
+        key: {
+            type: String,
+            required: true,
+            enum: ["A", "B", "C", "D"]
+        },
+
+        text: {
+            type: bilingualSchema,
+            required: true
+        },
+
+        weight: {
+            type: Number,
+            required: true
+        }
+    },
+    { _id: false }
+);
+
+
+
+const questionSchema = new Schema(
+  {
+    subjectID: {
+      type: Schema.Types.ObjectId,
+      ref: "Subject",
+    },
+    topicID: {
+      type: Schema.Types.ObjectId,
+      ref: "Topic",
+    },
+    subTopic: {
+      type: Schema.Types.ObjectId,
+      ref: "SubTopic",
+    },
+    question: {
+      type: bilingualSchema,
+      required: true,
+    },
+    option: {
+      type: [optionSchema],
+      required: true,
+      validate: {
+        validator: function (option) {
+          return option.length === 4;
+        },
+        message: "A question must have exactly 4 options",
+      },
+    },
+    explaination: { 
+        type: bilingualSchema,
+        required: true
+    }
+  },
+  { timestamps: true },
 );
 
 export const Question = mongoose.model("Question", questionSchema);
